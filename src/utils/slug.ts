7415cc7572de1ee1d72f5c3d669512d192a317ce
Maston <22679886+mastondzn@users.noreply.github.com>
2023-11-20 22:31:39 +0200

    move nanoid alphabet to top scope in attempt to reduce cpu time

--- src/utils/slug.ts
+++ src/utils/slug.ts
@@ -2,8 +2,8 @@ import { customAlphabet } from 'nanoid/non-secure';
 
 export const slugAlphabet = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz';
 export const slugRegex = new RegExp(`^[${slugAlphabet}]{0,12}$`);
+const nanoid = customAlphabet(slugAlphabet);
 
 export const generateSlug = (length: number = 4) => {
-    // this equates to 62^5 = ~916000000 possible combinations
-    return customAlphabet(slugAlphabet)(length);
+    return nanoid(length);
 };
