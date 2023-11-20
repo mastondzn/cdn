21ec937bb84b68a06585d2d5ded2f64efe001e72
Maston <22679886+mastondzn@users.noreply.github.com>
2023-11-20 11:32:58 +0200

    add to export star

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,2 +1,4 @@
-export { route as content } from './images';
-export { route as image } from './upload-image';
+export { route as images } from './images';
+export { route as uploadImage } from './upload-image';
+export { route as files } from './files';
+export { route as uploadFile } from './upload-file';
