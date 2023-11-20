ae282b21c72ff4b11d7f1a0a238ea9280f5b133e
Maston <22679886+mastondzn@users.noreply.github.com>
2023-11-20 09:50:02 +0200

    Update generateSlug function comment

--- src/utils/slug.ts
+++ src/utils/slug.ts
@@ -4,6 +4,6 @@ export const slugAlphabet = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmno
 export const slugRegex = new RegExp(`^[${slugAlphabet}]{0,12}$`);
 
 export const generateSlug = (length: number = 5) => {
-    // this equates to 62^5 = 9.16132832e+8 possible combinations
+    // this equates to 62^5 = ~916000000 possible combinations
     return customAlphabet(slugAlphabet)(length);
 };
