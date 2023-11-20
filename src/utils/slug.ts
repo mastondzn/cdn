f1963b94493c7b16dfffbbbe32721e5e9dfd146c
Maston <22679886+mastondzn@users.noreply.github.com>
2023-11-20 12:35:12 +0200

    reduce slug size

--- src/utils/slug.ts
+++ src/utils/slug.ts
@@ -3,7 +3,7 @@ import { customAlphabet } from 'nanoid/non-secure';
 export const slugAlphabet = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz';
 export const slugRegex = new RegExp(`^[${slugAlphabet}]{0,12}$`);
 
-export const generateSlug = (length: number = 5) => {
+export const generateSlug = (length: number = 4) => {
     // this equates to 62^5 = ~916000000 possible combinations
     return customAlphabet(slugAlphabet)(length);
 };
