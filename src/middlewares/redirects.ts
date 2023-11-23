84112c4264239fbd851574feabd4111c26ae5fb5
Maston <22679886+mastondzn@users.noreply.github.com>
2023-11-23 19:44:13 +0200

    oops

--- src/middlewares/redirects.ts
+++ src/middlewares/redirects.ts
@@ -2,9 +2,11 @@ import { defineMiddleware } from '~/utils';
 
 export const middleware = defineMiddleware({
     path: '*',
-    handler: async (ctx) => {
+    handler: async (ctx, next) => {
         if (ctx.req.path.startsWith('/files/')) {
             return ctx.redirect(new URL(ctx.req.url).pathname.replace('/files/', '/f/'));
         }
+
+        await next();
     },
 });
