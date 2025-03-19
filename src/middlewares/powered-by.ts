233d411b586d9fcafad35464f1e1231a342e7185
Maston <22679886+mastondzn@users.noreply.github.com>
2025-03-19 10:07:14 +0200

    fix: immutable headers

--- src/middlewares/powered-by.ts
+++ src/middlewares/powered-by.ts
@@ -2,5 +2,7 @@ import { middleware } from '~/utils/middleware';
 
 export const poweredBy = middleware(async (ctx, next) => {
     await next();
-    ctx.res.headers.set('x-powered-by', 'mastondzn/cdn (https://github.com/mastondzn/cdn)');
+    const headers = new Headers(ctx.res.headers);
+    headers.set('x-powered-by', 'mastondzn/cdn (https://github.com/mastondzn/cdn)');
+    ctx.res.headers = headers;
 });
