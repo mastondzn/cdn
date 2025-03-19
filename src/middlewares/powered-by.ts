40db15607849e1872283ac0a0166eb2d9329c9e9
Maston <22679886+mastondzn@users.noreply.github.com>
2025-03-19 10:12:34 +0200

    fix: xdd

--- src/middlewares/powered-by.ts
+++ src/middlewares/powered-by.ts
@@ -2,7 +2,11 @@ import { middleware } from '~/utils/middleware';
 
 export const poweredBy = middleware(async (ctx, next) => {
     await next();
-    const response = ctx.res.clone();
-    response.headers.set('x-powered-by', 'mastondzn/cdn (https://github.com/mastondzn/cdn)');
-    return response;
+    const headers = new Headers(ctx.res.headers);
+    headers.set('x-powered-by', 'mastondzn/cdn (https://github.com/mastondzn/cdn)');
+    return new Response(ctx.res.body, {
+        status: ctx.res.status,
+        statusText: ctx.res.statusText,
+        headers,
+    });
 });
